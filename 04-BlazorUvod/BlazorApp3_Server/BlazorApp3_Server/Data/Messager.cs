8779932cef48859086beb5fa3ab73f62d8bc6746
PetrVobornik <vobornik.petr@sspvc.cz>
2022-11-14 09:17:01 +0100

    Úvod do Blazoru (14.11.2022)

---
+++ 04-BlazorUvod/BlazorApp3_Server/BlazorApp3_Server/Data/Messager.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorApp3_Server.Data
+{
+    public class Messager
+    {
+        private Messager() { }
+
+        private static Messager current;
+        public static Messager Current
+            => current ?? (current = new Messager());
+
+        public event EventHandler OnMessage;
+
+        public void Changed()
+        {
+            OnMessage?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
