e9e65bf27ed9e210e737a94d7cc46326a9626c0d
PetrVobornik <vobornik.petr@sspvc.cz>
2022-11-21 09:21:11 +0100

    Blazor WASM - Hledání min (21.11.2022)

---
+++ 05-BlazorWASMHledaniMin/BlazorMiny/BlazorMiny/Code/Bunka.cs
@@ -0,0 +1,49 @@
+﻿namespace BlazorMiny.Code;
+
+public class Bunka
+{
+    public bool Odkryto { get; private set; } = false;
+    public bool Oznaceno { get; private set; } = false;
+    public bool Klikatelne => !Odkryto && !Oznaceno;
+
+    public bool Vysvitit { get; private set; } = false;
+    public string CssClass =>
+        (Vysvitit ? " svitit" : "") +
+        (Typ == 9 ? " mina" : "") +
+        (Typ > 0 && Typ < 9 ? $" cislo-{Typ}" : "");
+
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    int typ = 0; // Typ pole (0 - nic, 1-8 - číslo, 9 - mina)
+    public int Typ { 
+        get => typ; 
+        set { if (value >= 0 && value <= 9) typ = value; }
+    }
+
+    public event EventHandler Odryti;
+
+    public Bunka(int y, int x, EventHandler odryti)
+    {
+        X = x;
+        Y = y;
+        Odryti += odryti;
+    }
+
+    public void Odkryj(bool spustitUdalost)
+    {
+        if (!Klikatelne) return;
+        Odkryto = true;
+        if (spustitUdalost)
+        {
+            if (Typ == 9) Vysvitit = true;
+            Odryti?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public void Oznac()
+    {
+        if (Odkryto) return;
+        Oznaceno = !Oznaceno;
+    }
+}
---
+++ 05-BlazorWASMHledaniMin/BlazorMiny/BlazorMiny/Code/HraciPole.cs
@@ -0,0 +1,89 @@
+﻿namespace BlazorMiny.Code;
+
+public enum StavHry { Nacita, Probiha, Vyhra, Prohra }
+
+public class HraciPole
+{
+    public Bunka[,] MinovePole { get; private set; }
+
+    public int Sirka { get; set; }
+    public int Vyska { get; set; }
+    public int PocetMin { get; set; }
+    public StavHry Stav { get; private set; } = StavHry.Nacita;
+
+    public void VytvorPole()
+    {
+        Stav = StavHry.Nacita;
+        // Vytvoření a umístění buněk
+        MinovePole = new Bunka[Vyska, Sirka];
+        for (int i = 0; i < Vyska; i++)
+            for (int j = 0; j < Sirka; j++)
+                MinovePole[i, j] = new Bunka(i, j, OdkrytiBunky);
+
+        // Rozmístění min
+        for (int k = 0; k < PocetMin; k++)
+        {
+            (int y, int x) = (Random.Shared.Next(Vyska), Random.Shared.Next(Sirka));
+            while (MinovePole[y, x].Typ == 9)
+                (y, x) = (Random.Shared.Next(Vyska), Random.Shared.Next(Sirka));
+            MinovePole[y, x].Typ = 9;
+
+            // Očíslovat buňky okolo miny
+            for (int i = Math.Max(y - 1, 0); i <= Math.Min(y + 1, Vyska - 1); i++)
+                for (int j = Math.Max(x - 1, 0); j <= Math.Min(x + 1, Sirka - 1); j++)
+                    if (MinovePole[i, j].Typ != 9)
+                        MinovePole[i, j].Typ++;
+        }
+        Stav = StavHry.Probiha;
+    }
+
+    Queue<Bunka> queue = new Queue<Bunka>();
+
+    void OdkrytiBunky(object sender, EventArgs e)
+    {
+        var bunka = (Bunka)sender;
+        // Kliknutí na 0 => odkrytí i sousedů
+        if (bunka.Typ == 0)
+        {
+            queue.Clear();
+            queue.Enqueue(bunka);
+            OdkryjBunkuASousedy();
+        }
+        // Při kliknutí na minu odkrýt všechny miny a prohra
+        else if (bunka.Typ == 9)
+        {
+            foreach (var b in MinovePole)
+                if (b.Typ == 9)
+                    b.Odkryj(false);
+            Stav = StavHry.Prohra;
+            return;
+        }
+
+        // Kontrola výhry - vše krom min je odkryto
+        foreach (var b in MinovePole)
+            if (!b.Odkryto && b.Typ != 9)
+                return;
+        Stav = StavHry.Vyhra;
+    }
+
+    private void OdkryjBunkuASousedy()
+    {
+        while (queue.Count > 0)
+        {
+            var b = queue.Dequeue();
+            if (b.Typ == 0)
+            {
+                b.Odkryj(false);
+                // Odkrytí sousedů
+                for (int i = Math.Max(b.Y - 1, 0); i <= Math.Min(b.Y + 1, Vyska - 1); i++)
+                    for (int j = Math.Max(b.X - 1, 0); j <= Math.Min(b.X + 1, Sirka - 1); j++)
+                        if (MinovePole[i, j] != b && 
+                            MinovePole[i, j].Klikatelne &&
+                            !queue.Contains(MinovePole[i, j]))
+                            queue.Enqueue(MinovePole[i, j]);
+            }
+            else 
+                b.Odkryj(false);
+        }
+    }
+}
