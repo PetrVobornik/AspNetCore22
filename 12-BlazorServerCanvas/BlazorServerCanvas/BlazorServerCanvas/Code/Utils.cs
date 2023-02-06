41d2f83db0ac8744f979e611cb794d3b870f0211
PetrVobornik <vobornik.petr@sspvc.cz>
2023-02-06 09:33:37 +0100

    Blazor WASM se SignalR (6.2.2023)

--- 12-BlazorServerCanvas/BlazorServerCanvas/BlazorServerCanvas/Code/Utils.cs
+++ 12-BlazorServerCanvas/BlazorServerCanvas/BlazorServerCanvas/Code/Utils.cs
@@ -3,7 +3,5 @@
 public static class Utils
 {
     public static String ColorToHex(System.Drawing.Color c)
-    {
-        return "#" + c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2");
-    }
+        => "#" + c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2");
 }
